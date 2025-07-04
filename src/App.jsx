a947062849b11ba05388cdab1105fcdfbeab9826
KarthickRamAlagar <karthickramalagar@gmail.com>
2025-07-04 22:05:01 +0530

    Fix: UI updates and bug fixes before redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import GridPage from './pages/GridPage'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<GridPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<GridPage />} />
+    </Routes>
   )
 }
 
